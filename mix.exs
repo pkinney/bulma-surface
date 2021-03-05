10320dad908a07651700c5e5e9e750620bc3c78b
Powell Kinney <628952+pkinney@users.noreply.github.com>
2021-03-05 12:08:26 -0600

    Bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bulma.MixProject do
   def project do
     [
       app: :bulma_surface,
-      version: "0.1.0",
+      version: "0.3.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
